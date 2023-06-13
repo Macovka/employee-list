b27c2d631f04174131300ab842d2c3469b8bbf58
Macovka <dmakkoveeva6@gmail.com>
2023-06-13 14:54:29 +0400

    input-styles

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -29,17 +29,15 @@
 <style scoped>
   input {
     display: block;
-    width: 80%;
-    padding: 0.5rem 0.75rem;
+    width: 100%;
+    margin: auto;
+    padding: 8px;
+    box-sizing: border-box;
     font-size: 1rem;
-    font-weight: 400;
-    line-height: 1.5;
     color: #495057;
     background-color: #fff;
-    background-clip: padding-box;
     border: 1px solid #ced4da;
     border-radius: 0.25rem;
-    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
   }
 
   input:focus {
