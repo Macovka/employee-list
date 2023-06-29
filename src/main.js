b53f7b477b76a6890192ce7c3256ccef5af935f9
Macovka <dmakkoveeva6@gmail.com>
2023-06-29 19:38:33 +0400

    vuex-added

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import BaseInput from "./components/UI/BaseInput.vue";
 import BaseButton from "./components/UI/BaseButton.vue";
+import store from "./store";
 
 const app = createApp(App);
 
 app.component('BaseInput', BaseInput);
 app.component('BaseButton', BaseButton);
+app.use(store);
 app.mount("#app");
---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: { 
+          
+  },
+  getters: { // = computed properties       
+  },
+  actions: { 
+
+  },
+  mutations: { // setting and updating the state       
+  },
+})
+
+export default store;
